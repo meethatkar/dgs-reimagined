2414abe34261cb2051cb96a9a260a3ced9a7cc95
meethatkar <meethatkar000@gmail.com>
2026-09-01 02:05:32 +0530

    feat: configs and file creation.

---
+++ src/utils/gsap.utils.js
@@ -0,0 +1,11 @@
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+// ScrollSmoother requires ScrollTrigger
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollSmoother, ScrollToPlugin);
+
+export { gsap, useGSAP, ScrollTrigger, ScrollSmoother, ScrollToPlugin };
